1434e6ea01241e3e69c7fff9d499705320ee0359
nikita58-hub <liska4893@gmail.com>
2026-01-14 19:42:13 +0400

    ASDASDA
    DASSA

---
+++ занятие 14.01.26.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+x = 250
+y = 250
+j = 1
+s = 1
+f = 1
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    win.fill((255, 255, 255))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        y -= 1
+    elif keys[pygame.K_s]:
+        y += 1
+    if keys[pygame.K_a]:
+        x -= 1
+    if keys[pygame.K_d]:
+        x += 1
+    if x >= 251:
+        x -= 0.5
+    if y >= 251:
+        y -= 0.5
+    if x <= 251:
+        x += 0.5
+    if y <= 251:
+        y += 0.5
+
+
+    pygame.draw.circle(win, (j, s, f), (x, y), 25)
+    pygame.display.update()
+    pygame.time.delay(2)
