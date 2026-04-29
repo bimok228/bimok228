19719f794b04bd8195bb1e79f2223947a61f757b
nikita58-hub <liska4893@gmail.com>
2026-04-29 19:48:16 +0400

    adssad
    dsad

--- проект.py
+++ проект.py
@@ -4,54 +4,92 @@ width = 1200
 height = 800
 
 win = pygame.display.set_mode((width, height))
-background_image = pygame.image.load("555e71fc3786f4d133fc2d77a1c81d1d (1).jpg").convert()
+background_image = pygame.image.load("desert.jpg").convert()
 background_scaled = pygame.transform.scale(background_image, (width, height))
-def load_image(name):
-    img = pygame.image.load("555e71fc3786f4d133fc2d77a1c81d1d (1).jpg")
-    img = img.convert()  # Конвертирует в нужный формат
-    colorkey = img.get_at((0, 0))
-    img.set_colorkey(colorkey)
-    return img
-class Circle:
-    def __init__(self, color, x, y, rad):
-        self.color = color
-        self.x = 100
-        self.y = 300
-        self.rad = rad
-        self.isJump = False
-        self.jumpCount = 10
 
-
-    def draw(self):
-        pygame.draw.circle(win, self.color, (self.x, self.y), self.rad)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('dinozavr.png')
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.center = (200, 470)
+        self.is_jump = False
+        self.jump_count = 10
+        self.vertical_velocity = 0
+        self.alive = True
 
     def move_by_keys(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            self.isJump = True
-        if self.isJump:
-            if self.jumpCount >= -10:
-                if self.jumpCount < 0:
-                    self.y += (self.jumpCount ** 2) // 2
-                else:
-                    self.y -= (self.jumpCount ** 2) // 2
-                self.jumpCount -= 1
-            else:
-                self.jumpCount = 10
-                self.isJump = False
+        if not self.alive:
+            return
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and not self.is_jump: 
+            self.is_jump = True
+            self.vertical_velocity = -8
+            self.jump_count = 15
+
+        if self.is_jump:
+            if self.jump_count >= -15:
+                if self.jump_count > 0:
+                    self.rect.y -= (self.jump_count ** 2) * 0.2
+                else:
+                    self.rect.y += (self.jump_count ** 2) * 0.2
+                self.jump_count -= 1
+            else:
+                self.is_jump = False
+                self.jump_count = 1
+                if self.rect.bottom > height:
+                    self.rect.bottom = height
+
+    def die(self):
+        self.alive = False
+        self.kill()
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('kactus.png')
+        self.image = pygame.transform.scale(self.image, (140, 140))
+        self.rect = self.image.get_rect()
+        self.rect.right = width + 100
+        self.rect.centery = 470
+        self.speed = 9
+
+    def update(self, player):
+        self.rect.x -= self.speed
+
+        if self.rect.right < 0:
+            self.rect.right = width + 100
+            self.speed += 0.2
 FPS = 60
 clock = pygame.time.Clock()
 
-krug = Circle((255, 0, 0), width / 2, height / 2, 30)
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+enemy_sprites = pygame.sprite.Group()
+enemy = Enemy()
+enemy_sprites.add(enemy)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
-    krug.draw()
-    krug.move_by_keys()
+    hits = pygame.sprite.spritecollide(player, enemy_sprites, False)  # False — не удаляем кактус
+
+    if hits and player.alive:
+        player.die()
+
+    win.blit(background_scaled, (0, 0))
+
+    player.move_by_keys()
+    enemy_sprites.update(player)
+
+    all_sprites.draw(win)
+    enemy_sprites.draw(win)
+
     pygame.display.update()
     clock.tick(FPS)
-    win.blit(background_scaled, (0, 0))
